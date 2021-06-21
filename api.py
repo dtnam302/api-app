6fe00d1b6e46ba6a1866339536cb3a400603985f
Doan Nam <dtnam302@gmail.com>
2021-06-21 10:45:00 +0700

    api app

---
+++ api.py
@@ -0,0 +1,24 @@
+import flask
+import json
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+f = open('test.json',)
+data = json.load(f)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/testchart/all', methods=['GET'])
+def api_all():
+    return jsonify(data)
+
+app.run()
